a4b60d945f93e2acce71469aa8dc646e26929646
Gregoire Humeau <gregoire@appventus.com>
2015-11-27 17:23:48 +0100

    Change cache and logs dir

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,4 +39,14 @@ class AppKernel extends Kernel
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
+
+    public function getCacheDir()
+    {
+        return $this->rootDir.'/../var/cache/'.$this->environment;
+    }
+
+    public function getLogDir()
+    {
+        return $this->rootDir.'/../var/logs';
+    }
 }
