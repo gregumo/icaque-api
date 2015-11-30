72d06596b364361e61851e0be9d89e836397aeb3
Vincent Chapron <vincent@chapron.io>
2015-11-30 10:33:49 +0100

    Override registration form type

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,30 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User',
+            'csrf_protection'   => false,
+        ));
+    }
+}
